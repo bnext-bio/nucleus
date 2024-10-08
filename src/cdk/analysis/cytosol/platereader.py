d0ea98e0f03a897ffc25800a48fcd933a3c8077b
Anton Jackson-Smith <anton@dyingstar.net>
2024-10-07 17:59:35 -0700

    Reformatted code, now passes linter.

--- src/cdk/analysis/cytosol/platereader.py
+++ src/cdk/analysis/cytosol/platereader.py
@@ -12,47 +12,99 @@ Usage:
 
 import io
 import re
-import glob
-import datetime
+import os.path
 
 import numpy as np
 import pandas as pd
 
-def read_glomax(data_dir: str) -> pd.DataFrame:
-    all_df = list()
 
-    # glob over .csv files in dfpath; append to data; concatenate into one DataFrame
-    for csv in glob.glob(f"{data_dir}/*.csv"):
-        df = pd.read_csv(csv)
-        df["File"] = os.path.basename(csv)
-        df["Row"] = df["WellPosition"].str.split(":").str[0]
-        df["Column"] = df["WellPosition"].str.split(":").str[1].astype(int) 
-        df["Time"] = pd.to_datetime(data["File"].str.replace(r'.* ([0-9.]+ [0-9_]+).*', r'\1', regex=True), format="%Y.%m.%d %H_%M_%S")
-        df["WellTime"] = pd.to_timedelta(data["Timestamp(ms)"], "us")
-    
-        data.append(df) 
-    
-    data = pd.concat(data, ignore_index=True)
+def load_platereader_data(data_file: str) -> pd.DataFrame:
+    """
+    Load plate reader data from a file and return a DataFrame.
 
-    # label different wavelengths
-    channel_map = dict(zip(data["ID"].unique(), ["A600", "Blue", "Green", "Red"]))
-    data["Channel"] = data["ID"].map(channel_map)
+    This function loads platereader data from a CSV file, parsing it into a standardized format and labelling
+    it with a provided plate map.
 
-    palette = dict(zip(dict.fromkeys(channel_map.values()), ["brown", "limegreen", "red", "firebrick"]))
+    Filenames should be formatted in a standard format: `[date]-[device]-[experiment].csv`. For
+    example, `20241004-envision-dna-concentration.csv`.
 
-    # massage Time
-    data["TimeDelta"] = data["Time"] - data["Time"].min()
-    data["TimeDeltaPretty"] = data["TimeDelta"].map(lambda x: "{:02d}:00".format(x.components.hours)) #{:02d}".format(x.components.hours, x.components.minutes))
+    Data is loaded based on the device field in the filename, which is used to determine the appropriate reader-specific
+    data parser. Currently supported readers are:
+    - BioTek Cytation 5: `cytation`
+    - Revvity Envision Nexus: `envision`
 
-    # Get a generic data column
-    data["Data"] = data["CalculatedFlux"]
-    data["Data"].fillna(data[data["CalculatedFlux"].isna()]["OpticalDensity"], inplace=True)
-    
-    # Label replicates
-    data["Replicate"] = data["File"].map(lambda x: re.sub(r'.* OUT ([0-9]+).csv', r'\1', x))
-    data.sort_values(by=["TimeDelta", "Row", "Column"], inplace=True)
+    Data is returned as a pandas DataFrame with the following mandatory columns:
+    - `Well`: Well identifier (e.g. `A1`)
+    - `Row`: Row identifier (e.g. `A`)
+    - `Column`: Column identifier (e.g. `1`)
+    - `Time`: Time of measurement
+    - `Seconds`: Time of measurement in seconds
+    - `Temperature (C)`: Temperature at time of measurement
+    - `Read`: A tag describing the type of measurement (e.g. `OD600`, `Fluorescence`). The format of this field is
+    currently device-specific.
+    - `Data`: The measured data value
 
-    return data
+    In addition, the provided platemap will be merged to the loaded data on the `Well` column. All other columns within
+    the platemap will be present in the returned dataframe.
+
+    Args:
+        data_file (str): Path to the plate reader data file.
+
+    Returns:
+        pd.DataFrame: DataFrame containing the plate reader data in a structured format.
+
+    """
+    filename = os.path.basename(data_file).lower()
+
+    if filename.startswith("cytation"):
+        return read_cytation(data_file)
+    elif filename.startswith("envision"):
+        return read_envision(data_file)
+    # elif filename_lower.startswith("glomax"):
+    #     return read_glomax(os.path.dirname(data_file))
+    else:
+        raise ValueError(f"Unsupported plate reader data file: {data_file}")
+
+
+# def read_glomax(data_dir: str) -> pd.DataFrame:
+#     # glob over .csv files in dfpath; append to data; concatenate into one DataFrame
+#     data = list()
+#     for csv in glob.glob(f"{data_dir}/*.csv"):
+#         df = pd.read_csv(csv)
+#         df["File"] = os.path.basename(csv)
+#         df["Row"] = df["WellPosition"].str.split(":").str[0]
+#         df["Column"] = df["WellPosition"].str.split(":").str[1].astype(int)
+#         df["Time"] = pd.to_datetime(
+#             data["File"].str.replace(r".* ([0-9.]+ [0-9_]+).*", r"\1", regex=True), format="%Y.%m.%d %H_%M_%S"
+#         )
+#         df["WellTime"] = pd.to_timedelta(data["Timestamp(ms)"], "us")
+
+#         data.append(df)
+
+#     data = pd.concat(data, ignore_index=True)
+
+#     # label different wavelengths
+#     channel_map = dict(zip(data["ID"].unique(), ["A600", "Blue", "Green", "Red"]))
+#     data["Channel"] = data["ID"].map(channel_map)
+
+#     palette = dict(zip(dict.fromkeys(channel_map.values()), ["brown", "limegreen", "red", "firebrick"]))
+
+#     # massage Time
+#     data["TimeDelta"] = data["Time"] - data["Time"].min()
+#     data["TimeDeltaPretty"] = data["TimeDelta"].map(
+#         lambda x: "{:02d}:00".format(x.components.hours)
+#     )  # {:02d}".format(x.components.hours, x.components.minutes))
+
+#     # Get a generic data column
+#     data["Data"] = data["CalculatedFlux"]
+#     data["Data"].fillna(data[data["CalculatedFlux"].isna()]["OpticalDensity"], inplace=True)
+
+#     # Label replicates
+#     data["Replicate"] = data["File"].map(lambda x: re.sub(r".* OUT ([0-9]+).csv", r"\1", x))
+#     data.sort_values(by=["TimeDelta", "Row", "Column"], inplace=True)
+
+#     return data
+
 
 def read_cytation(data_file: str, sep="\t") -> pd.DataFrame:
     # read data file as long string
@@ -66,15 +118,11 @@ def read_cytation(data_file: str, sep="\t") -> pd.DataFrame:
     readidx = re.search(r"^(Read\s)?\d+,\d+", data, re.MULTILINE)
 
     # get header DataFrame
-    header = data[:procidx.start()]
-    header = pd.read_csv(
-        io.StringIO(header), 
-        delimiter=sep, 
-        header=0, names=["key", "value"]
-    )
+    header = data[: procidx.start()]
+    header = pd.read_csv(io.StringIO(header), delimiter=sep, header=0, names=["key", "value"])
 
     # get procedure DataFrame
-    procedure = data[procidx.end():layoutidx.start()]  
+    procedure = data[procidx.end():layoutidx.start()]
     procedure = pd.read_csv(io.StringIO(procedure), skipinitialspace=True, names=range(4))
     procedure = procedure.replace(np.nan, "")
 
@@ -86,13 +134,13 @@ def read_cytation(data_file: str, sep="\t") -> pd.DataFrame:
 
     # iterate over data string to find individual reads
     reads = dict()
-    
+
     sep = r"(?:Read\s\d+:)?(?:\s\d{3},\d{3}(?:\[\d\])?)?" + sep
-        
+
     for readidx in re.finditer(r"^(Read\s)?\d+,\d+.*\n", data, re.MULTILINE):
         # for each iteration, extract string from start idx to end icx
         read = data[readidx.end():]
-        read = read[:re.search(r"(^(Read\s)?\d+,\d+|^Blank Read\s\d|Results|\Z)", read[1:], re.MULTILINE).start()]
+        read = read[: re.search(r"(^(Read\s)?\d+,\d+|^Blank Read\s\d|Results|\Z)", read[1:], re.MULTILINE).start()]
         read = pd.read_csv(io.StringIO(read), sep=sep, engine="python").convert_dtypes()
         reads[data[readidx.start():readidx.end()].strip()] = read
 
@@ -102,26 +150,27 @@ def read_cytation(data_file: str, sep="\t") -> pd.DataFrame:
     for name, r in reads.items():
         # filter out Cytation calculated kinetic parameters, which are cool, but don't want rn
         r = r[r.Time.str.contains(r"\d:\d{2}:\d{2}", regex=True)]
-        
+
         # extract meaningful parameters from really big string
         r = r.melt(id_vars=["Time", "T°"], var_name="Well", value_name="Data")
-        r['Row'] = r['Well'].str.extract(r"([A-Z]+)")
-        r['Column'] = r['Well'].str.extract(r"(\d+)").astype(int)
-        r['Temperature (C)'] = r['T°'].str.extract(r"(\d+)").astype(float)
-        r['Data'] = r['Data'].replace("OVRFLW", np.inf)
-        r['Data'] = r['Data'].astype(float)
-        r['Read'] = name
-        r['Ex'] = r['Read'].str.extract(r"(\d+),\d+").astype(int)
-        r['Em'] = r['Read'].str.extract(r"\d+,(\d+)").astype(int)
+        r["Row"] = r["Well"].str.extract(r"([A-Z]+)")
+        r["Column"] = r["Well"].str.extract(r"(\d+)").astype(int)
+        r["Temperature (C)"] = r["T°"].str.extract(r"(\d+)").astype(float)
+        r["Data"] = r["Data"].replace("OVRFLW", np.inf)
+        r["Data"] = r["Data"].astype(float)
+        r["Read"] = name
+        r["Ex"] = r["Read"].str.extract(r"(\d+),\d+").astype(int)
+        r["Em"] = r["Read"].str.extract(r"\d+,(\d+)").astype(int)
         read_dataframes.append(r)
 
     data = pd.concat(read_dataframes)
 
     # add time column to data DataFrame
-    data['Time'] = pd.to_timedelta(data['Time']).astype('timedelta64[s]')
-    data['Seconds'] = data['Time'].map(lambda x: x.total_seconds())
+    data["Time"] = pd.to_timedelta(data["Time"]).astype("timedelta64[s]")
+    data["Seconds"] = data["Time"].map(lambda x: x.total_seconds())
 
-    return data[['Well', 'Row', 'Column', 'Time', 'Seconds', 'Temperature (C)', 'Read', 'Data']]
+    return data[["Well", "Row", "Column", "Time", "Seconds", "Temperature (C)", "Read", "Data"]]
+
 
 def read_envision(data_file: str) -> pd.DataFrame:
     # load data
@@ -132,17 +181,17 @@ def read_envision(data_file: str) -> pd.DataFrame:
     data["Column"] = data["Well ID"].apply(lambda s: str(int(s[1:])))
     data["Well"] = data.apply(lambda well: f"{well['Row']}:{well['Column']}", axis=1)
 
-    data['Time'] = pd.to_timedelta(data['Time [hhh:mm:ss.sss]']).astype('timedelta64[s]')
-    data['Seconds'] = data['Time'].map(lambda x: x.total_seconds())
+    data["Time"] = pd.to_timedelta(data["Time [hhh:mm:ss.sss]"]).astype("timedelta64[s]")
+    data["Seconds"] = data["Time"].map(lambda x: x.total_seconds())
 
-    data['Temperature (C)'] = data['Temperature current[°C]']
+    data["Temperature (C)"] = data["Temperature current[°C]"]
 
-    data['Read'] = data['Operation']
+    data["Read"] = data["Operation"]
 
     data["Data"] = data["Result Channel 1"]
 
     data["Excitation (nm)"] = data["Exc WL[nm]"]
     data["Emission (nm)"] = data["Ems WL Channel 1[nm]"]
     data["Wavelength (nm)"] = data["Excitation (nm)"] + "," + data["Emission (nm)"]
-        
-    return data[['Well', 'Row', 'Column', 'Time', 'Seconds', 'Temperature (C)', 'Read', 'Data']]
+
+    return data[["Well", "Row", "Column", "Time", "Seconds", "Temperature (C)", "Read", "Data"]]
--- tests/unit/analysis/cytosol/test_platereader.py
+++ tests/unit/analysis/cytosol/test_platereader.py
@@ -1,16 +1,7 @@
 import pytest
-import os.path
 
 CYTATION_KINETIC_DATA_PATH = "tests/test_data/cytation_fluorescence_kinetics.txt"
 ENVISION_KINETIC_DATA_PATH = "tests/test_data/envision_fluorescence_kinetics.csv"
-
-@pytest.fixture(params=['envision', 'cytation'])
-def platereader_data(request):
-    from cdk.analysis.cytosol.platereader import read_envision, read_cytation
-    if request.param == 'envision':
-        return read_envision(ENVISION_KINETIC_DATA_PATH)
-    elif request.param == 'cytation':
-        return read_cytation(CYTATION_KINETIC_DATA_PATH)
 
 
 def test_read_envision():
@@ -22,32 +13,52 @@ def test_read_envision():
 
     assert isinstance(data, pd.DataFrame)
 
+
 def test_read_cytation():
     import pandas as pd
     from cdk.analysis.cytosol.platereader import read_cytation
-    
+
     data = read_cytation(CYTATION_KINETIC_DATA_PATH)
     print(f"Columns: {data.columns}")
 
     assert isinstance(data, pd.DataFrame)
 
+
+@pytest.fixture(params=["envision", "cytation"])
+def platereader_data(request):
+    from cdk.analysis.cytosol.platereader import read_envision, read_cytation
+
+    if request.param == "envision":
+        return read_envision(ENVISION_KINETIC_DATA_PATH)
+    elif request.param == "cytation":
+        return read_cytation(CYTATION_KINETIC_DATA_PATH)
+
+
 def test_platereader_read_has_mandatory_columns(platereader_data):
     data = platereader_data
     required_columns = {"Well", "Row", "Column", "Time", "Seconds", "Temperature (C)", "Read", "Data"}
-    assert required_columns.issubset(set(data.columns)), f"Missing required columns. Expected {required_columns} to be a subset of {set(data.columns)}"
+    assert required_columns.issubset(
+        set(data.columns)
+    ), f"Missing required columns. Expected {required_columns} to be a subset of {set(data.columns)}"
+
 
 def test_platereader_read_mandatory_columns_are_ordered(platereader_data):
     data = platereader_data
     required_columns = ["Well", "Row", "Column", "Time", "Seconds", "Temperature (C)", "Read", "Data"]
-    assert (required_columns == data.columns.values[:len(required_columns)]).all(), f"Missing required columns. Expected {required_columns} to be a subset of {set(data.columns)}"
-    # assert data.shape == (96, 18)
-    # assert set(data.columns) == set([
-    #     'Well ID', 'Result Channel 1', 'Exc WL[nm]', 'Ems WL Channel 1[nm]', 
-    #     'Time [hhh:mm:ss.sss]', 'Repeat', 'Row', 'Column', 'Well', 'Data', 
-    #     'Ex', 'Em', 'Wavelength', 'Time', 'Seconds', 'Background', 
-    #     'BackgroundSubtracted'
-    # ])
+    assert (
+        required_columns == data.columns.values[: len(required_columns)]
+    ).all(), f"Missing required columns. Expected {required_columns} to be a subset of {set(data.columns)}"
 
-    # assert data['Data'].dtype == float
-    # assert data['Time'].dtype == 'timedelta64[s]'
-    # assert data['Seconds'].dtype == float
+
+@pytest.fixture(params=[ENVISION_KINETIC_DATA_PATH, CYTATION_KINETIC_DATA_PATH])
+def platereader_data_file(request):
+    return request.param
+
+
+def test_load_platereader_data(platereader_data_file):
+    import pandas as pd
+    from cdk.analysis.cytosol.platereader import load_platereader_data
+
+    data = load_platereader_data(platereader_data_file)
+
+    assert isinstance(data, pd.DataFrame)
